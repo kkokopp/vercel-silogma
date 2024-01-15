4df542d3af81e02bbc8b2944060567bbedffce35
Kopriyanto <kopriyan003@gmail.com>
2024-01-15 22:50:15 +0700

    edit vite

--- resources/views/guest/layouts/app.blade.php
+++ resources/views/guest/layouts/app.blade.php
@@ -40,8 +40,8 @@
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script> --}}
     {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
     <script src="{{ asset('js/dropdown.js') }}"></script>
-    <script src="{{ mix('resources/js/uploadImage.js') }}"></script>
-    <script src="{{ mix('resources/js/riwayatForm.js') }}"></script>
+    @vite('resources/js/uploadImage.js')
+    @vite('resources/js/riwayatForm.js')
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script> --}}
 </body>
 
